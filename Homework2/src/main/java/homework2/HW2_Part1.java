f2455d9afe27a1f3f2634ca728509187f02dad9f
cloudera <abhinav.swami21@gmail.com>
2016-11-26 21:47:16 -0800

    Initial commit

---
+++ Homework2/src/main/java/homework2/HW2_Part1.java
@@ -0,0 +1,270 @@
+package homework2;
+
+import java.util.Calendar;
+import java.util.regex.Pattern;
+
+import org.apache.spark.Accumulator;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+import scala.Tuple3;
+
+/**
+ * Part 1 - Extract the information we want from the file
+ * 
+ * Use an accumulator to count all the valid and invalid records in the file for
+ * the S&P 500
+ * 
+ * List the <Symbol, Dividend Yield, Price/Earning> information as output.
+ * 
+ * Make sure you filter out anything that is not a valid entry
+ * 
+ * ---------------------------------------
+ * 
+ * Input: companies/SP500-constituents-financials.csv
+ * 
+ * **Look at the file**, the first line is a header, listing the information in
+ * each column
+ * 
+ * Output a list containing a Tuple3 of <Symbol, Dividend Yield, Price/Earnings>
+ * 
+ * Output the number of valid records in the file - is it 500?
+ * 
+ * ----------------------------------------
+ * 
+ * @author Abhinav Swami
+ *
+ */
+public class HW2_Part1 {
+
+	private final static String recordRegex = ",";
+	private final static Pattern REGEX = Pattern.compile(recordRegex);
+
+	/*
+	 * TODO initialize the indices for parsing - symbol is at index 0, dividend
+	 * is at index 4, price-earnings is at index 5
+	 */
+
+	/*
+	 * TODO initialize a String for representing non-existent information
+	 * 
+	 * In the file, the dividend and price-to-earnings information may be blank
+	 * ("")
+	 */
+
+	/*
+	 * TODO initialize a String for representing the header
+	 * 
+	 * - for instance, you can check the symbol field, if it equals "Symbol" you
+	 * know you are parsing the header
+	 */
+
+	/*
+	 * You may want a testing flag so you can switch off debugging information
+	 * easily
+	 */
+	private static boolean testing = true;
+
+	/**
+	 * In main I have supplied basic information for starting the job in
+	 * Eclipse.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Validate that two arguments were passed from the command line.
+		 */
+		if (args.length != 2) {
+			System.out.println("Arguments provided:  ");
+			for (String arg : args) {
+				System.out.println(arg);
+			}
+			System.out.printf("Usage: Provide <input dir> <output dir> \n");
+			System.out.printf("Example: data/companies/SP500-constituents-financials.csv output/hw2_1 \n");
+			System.exit(-1);
+		}
+
+		/*
+		 * setup job configuration and context
+		 */
+		SparkConf conf = new SparkConf().set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+		conf.setMaster("local");
+		conf.setAppName("HW2 Part 1");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		/*
+		 * setup output
+		 */
+		String outputPath = args[1] + "_" + Calendar.getInstance().getTimeInMillis();
+
+		/*
+		 * TODO setup accumulators for counting records - for instance, you may
+		 * want to count valid records, invalid records, the number of records
+		 * with no dividend supplied, etc.
+		 */
+
+		final Accumulator<Integer> validRecords = sc.accumulator(0);
+		final Accumulator<Integer> invalidRecords = sc.accumulator(0);
+		final Accumulator<Integer> noDivRecords = sc.accumulator(0);
+		/*
+		 * Read the lines in a file
+		 */
+		JavaRDD<String> lines = sc.textFile(args[0]);
+
+		/*
+		 * if testing, cache the file and then check out the first ten lines in
+		 * the file
+		 */
+		if (testing) {
+			/*
+			 * Show the header - notice, it is the first line in the file, so we
+			 * can use first() - an action.
+			 * 
+			 * Data that is not cached is discarded after an action, so we
+			 * should cache here or Spark will read the file again to recreate
+			 * lines for the next action.
+			 */
+			lines.cache();
+			// System.out.println(lines.first());
+		}
+
+		/*
+		 * - TODO use map to parse each line to a Tuple3 containing <Symbol,
+		 * Dividend Yield, Price/Earnings>
+		 * 
+		 * Notice: There are 15 fields, separated by commas, on each line of the
+		 * file
+		 * 
+		 * Your map function may start like this:
+		 * 
+		 * JavaRDD<Tuple3<String, String, String>> stockInfo = lines .map(new
+		 * Function<String, Tuple3<String, String, String>>() {
+		 */
+
+		JavaRDD<Tuple3<String, String, String>> stockInfo = lines
+				.map(new Function<String, Tuple3<String, String, String>>() {
+
+					private final static String recordRegex = ",";
+					private final static int symbolIndex = 0;
+					private final static int divYield = 4;
+					private final static int priceByEarning = 5;
+
+					@Override
+					public Tuple3<String, String, String> call(String s) throws Exception {
+						String[] tokens = s.split(recordRegex, -1);
+						if (tokens.length != 15 || tokens[divYield].equals("") || tokens[priceByEarning].equals(""))
+							invalidRecords.add(1);
+
+						if (tokens[divYield].equals("")) {
+							tokens[divYield] = new String("0.0");
+						}
+						if (tokens[priceByEarning].equals("")) {
+							tokens[priceByEarning] = String.valueOf(Float.NEGATIVE_INFINITY);
+						}
+
+						return new Tuple3<String, String, String>(tokens[symbolIndex], tokens[divYield],
+								tokens[priceByEarning]);
+					}
+
+				});
+
+		/*-
+		 * TODO Filter out invalid records 
+		 * 
+		 * - filter out bad fields - dividend and price-earning fields that don't contain floats
+		 * - filter out the header
+		 * 
+		 * - use the filter as an opportunity to count valid and invalid records
+		 *
+		 * Your filter may start like this:
+		 * 
+		 * JavaRDD<Tuple3<String, String, String>> filteredInfo = stockInfo
+		 *		.filter(new Function<Tuple3<String, String, String>, Boolean>() {
+		 */
+		JavaRDD<Tuple3<String, String, String>> filteredInfo = stockInfo
+				.filter(new Function<Tuple3<String, String, String>, Boolean>() {
+
+					@Override
+					public Boolean call(Tuple3<String, String, String> s) throws Exception {
+
+						// if dividend is not a float value, count it as invalid
+						// record and filter it out
+						if (!Pattern.matches("([0-9]*)\\.([0-9]*)", s._2())) {
+							invalidRecords.add(1);
+							return false;
+						}
+
+						// if price/earning is not a float value, count it as
+						// invalid record and filter it out
+						if (!(s._3().equals(String.valueOf(Float.NEGATIVE_INFINITY))
+								|| Pattern.matches("([0-9]*)\\.([0-9]*)", s._3()))) {
+							invalidRecords.add(1);
+							return false;
+						}
+
+						// Filter out header
+						if (s._1().equals("Symbol")) {
+							invalidRecords.add(1);
+							return false;
+
+						}
+
+						validRecords.add(1);
+						return true;
+					}
+				});
+
+		System.out.println(filteredInfo.count());
+		System.out.println("Total number of entries = " + validRecords.value());
+		System.out.println("Invalid entries = " + invalidRecords.value());
+		/*-
+		 * TODO You may want to sort the tuples before you write them to file.
+		 * 
+		 * To do so, you can use sortBy using the first element, symbol, found in filteredInfo._1()
+		 * 
+		 * This is just so the output list is easier to read/decipher. Note, it does initiate a "wide-transformation"
+		 * 
+		 * --- this sortBy implementation worked for me... -------------
+		 *
+		 * JavaRDD<Tuple3<String, String, String>> sortedInfo = filteredInfo
+		 *		.sortBy(new Function<Tuple3<String, String, String>, String>() {
+		 *
+		 *			@Override
+		 *			public String call(Tuple3<String, String, String> info) throws Exception {
+		 *				return info._1();
+		 *			}
+		 *
+		 *		}, true, 1);
+		 */
+
+		JavaRDD<Tuple3<String, String, String>> sortedInfo = filteredInfo
+				.sortBy(new Function<Tuple3<String, String, String>, String>() {
+
+					@Override
+					public String call(Tuple3<String, String, String> info) throws Exception {
+						return info._1();
+					}
+				}, true, 1);
+		/*-
+		 * and action! 
+		 * TODO  write the information out to a file using "saveAsTextFile"
+		 * 
+		 */
+
+		sortedInfo.saveAsTextFile(outputPath);
+		/*
+		 * TODO Now that an action has run, the accumulators will be defined You
+		 * can view them using something like this:
+		 * 
+		 * System.out.println("Valid records:  " + validRecords.value());
+		 */
+
+		/*
+		 * bye
+		 */
+		sc.close();
+	}
+}
---
+++ Homework2/src/main/java/homework2/HW2_Part2.java
@@ -0,0 +1,156 @@
+package homework2;
+
+import java.util.Calendar;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+import scala.Tuple3;
+
+public class HW2_Part2 {
+
+	public static void main(String[] args) {
+		/*
+		 * Validate that two arguments were passed from the command line.
+		 */
+		if (args.length != 3) {
+			System.out.println("Arguments provided:  ");
+			for (String arg : args) {
+				System.out.println(arg);
+			}
+			System.out.printf("Usage: Provide <input dir1> <input dir2> <output dir> \n");
+			System.out.printf(
+					"Example: data/companies/SP500-constituents-financials.csv data/companies/companylistNASDAQ.csv output/hw2_1 \n");
+			System.exit(-1);
+		}
+
+		/*
+		 * setup job configuration and context
+		 */
+		SparkConf conf = new SparkConf().set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+		conf.setMaster("local");
+		conf.setAppName("HW2 Part 1");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		/* setup output */
+		String outputPath = args[2] + "_" + Calendar.getInstance().getTimeInMillis();
+
+		/* Read the lines from SP500-constituents-financials.csv */
+		JavaRDD<String> rdd1 = sc.textFile(args[0]);
+
+		/* Read the lines from companylistNASDAQ.csv */
+		JavaRDD<String> rdd2 = sc.textFile(args[1]);
+
+		// Parse out key, value pairs from the csv file
+		JavaPairRDD<String, String> info1 = rdd1.mapToPair(new PairFunction<String, String, String>() {
+
+			private final static String recordRegex = ",";
+			private final static int symbolIndex = 0;
+
+			@Override
+			public Tuple2<String, String> call(String s) throws Exception {
+				String[] tokens = s.split(recordRegex, -1);
+				return new Tuple2<String, String>(tokens[symbolIndex], s.replace(tokens[symbolIndex] + ",", ""));
+			}
+
+		});
+
+		// Parse out key, value pairs from the second csv file
+		JavaPairRDD<String, String> info2 = rdd2.mapToPair(new PairFunction<String, String, String>() {
+
+			private final static String recordRegex = ",";
+			private final static int symbolIndex = 0;
+
+			@Override
+			public Tuple2<String, String> call(String s) throws Exception {
+				String str = s.replace("\"", ""); // remove all quotes from
+													// string
+				String[] tokens = str.split(recordRegex, -1);
+				return new Tuple2<String, String>(tokens[symbolIndex], str.replace(tokens[symbolIndex] + ",", ""));
+			}
+
+		});
+
+		// Filter out the header
+		JavaPairRDD<String, String> filteredInfo1 = info1.filter(new Function<Tuple2<String, String>, Boolean>() {
+
+			@Override
+			public Boolean call(Tuple2<String, String> s) throws Exception {
+
+				// Filter out header
+				if (s._1().equals("Symbol")) {
+					return false;
+				}
+				return true;
+			}
+		});
+
+		// Combine results based on keys. Select key present in both rdd1 and
+		// rdd2
+		JavaPairRDD<String, Tuple2<String, String>> joined = filteredInfo1.join(info2);
+
+		// Update dividend, 52 week high and 52 week low values
+		JavaRDD<Tuple3<String, Double, Double>> stockInfo = joined
+				.map(new Function<Tuple2<String, Tuple2<String, String>>, Tuple3<String, Double, Double>>() {
+
+					private final static String recordRegex = ",";
+					private final static int symbolIndex = 0;
+					private final static int divYield = 4;
+					private final static int priceByEarning = 5;
+
+					@Override
+					public Tuple3<String, Double, Double> call(Tuple2<String, Tuple2<String, String>> tup)
+							throws Exception {
+						String[] tokens = tup._2._1.split(recordRegex, -1);
+						String dividend = tokens[3];
+						String low = tokens[7];
+						String high = tokens[8];
+						if (dividend.equals("n/a") || dividend.equals(""))
+							dividend = "0.0";
+						if (low.equals("n/a"))
+							low = "0.0";
+						if (high.equals("n/a"))
+							high = "0.0";
+						Double increase = (Double.parseDouble(high) - Double.parseDouble(low))
+								/ Double.parseDouble(low);
+						return new Tuple3<String, Double, Double>(tup._1, Double.parseDouble(dividend), increase);
+					}
+
+				});
+
+		// Sort by growth
+		JavaRDD<Tuple3<String, Double, Double>> sortedByGrowth = stockInfo
+				.sortBy(new Function<Tuple3<String, Double, Double>, Double>() {
+
+					@Override
+					public Double call(Tuple3<String, Double, Double> info) throws Exception {
+						return info._3();
+					}
+				}, false, 1);
+
+		// Sort by highest dividend value
+		JavaRDD<Tuple3<String, Double, Double>> sortedByDiv = stockInfo
+				.sortBy(new Function<Tuple3<String, Double, Double>, Double>() {
+
+					@Override
+					public Double call(Tuple3<String, Double, Double> info) throws Exception {
+						return info._2();
+					}
+				}, false, 1);
+
+		// Print the results
+		System.out.println("Total symbols available in both the files = " + stockInfo.count());
+		System.out.println(sortedByGrowth.take(10));
+		System.out.println(sortedByDiv.take(10));
+
+		// Save the results
+		sortedByGrowth.saveAsTextFile(outputPath);
+		sc.close();
+	}
+
+}
